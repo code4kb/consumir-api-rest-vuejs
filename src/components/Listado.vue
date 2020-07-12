40fbd66496fa33cabf07b3ed8a02ac000711617a
Gabriel Rodriguez <code4kb@gmail.com>
2020-07-11 22:25:18 -0300

    - Mostramos los datos en  la vista

--- src/components/Listado.vue
+++ src/components/Listado.vue
@@ -1,42 +1,62 @@
 <template>
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Usuario</th>
-            <th scope="col">Título</th>
-            <th scope="col">Estado</th>
-        </tr>
-    </thead>
-    <tbody>
-    <tr>
-        <th scope="row">1</th>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-    </tbody>
-</table>
+
+<section v-if="huboerror">
+    <p>Lo sentimos, no es posible obtener la información en este momento, por favor intente nuevamente mas tarde.</p>
+</section>
+
+<section v-else>
+    <div v-if="cargando">Cargando...</div>
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Usuario</th>
+                    <th scope="col">Título</th>
+                    <th scope="col">Estado</th>
+                </tr>
+            </thead>
+            <tbody>
+            <tr v-for="todo in todos" :key="todo.id">
+                <th scope="row">{{todo.id}}</th>
+                <td>{{todo.userId}}</td>
+                <td>{{todo.title}}</td>
+                <td>{{todo.completed}}</td>
+            </tr>
+            </tbody>
+        </table>
+    </section>    
 </template>
 
 <script>
-//import axios from 'axios'
+import axios from 'axios'
 
 export default {
     name: 'listado',
     data() {
         return {
-            all:null
+            todos:null,
+            cargando: true,
+            huboerror: false,
+            urlApi: 'https://jsonplaceholder.typicode.com/todos'
         }
     },
     mounted(){
-        console.log(" --- mounted() ---");
         this.getAll();
     },
     methods: {
         getAll(){
-            console.log(" --- getAll() ---");
-        }
+            axios
+                .get(this.urlApi)
+                .then(response => {
+                    this.todos = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                    this.huboerror = true
+                })
+                .finally(() => this.cargando = false)
+                }
     }
 }
 </script>
