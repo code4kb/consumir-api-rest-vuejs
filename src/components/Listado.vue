68dff45e2c8c00edd34a49b5502a99021a24cbee
Gabriel Rodriguez <code4kb@gmail.com>
2020-07-15 22:28:11 -0300

    - Agrego un spinner y mejoro el codigo del v-for

--- src/components/Listado.vue
+++ src/components/Listado.vue
@@ -7,13 +7,9 @@
     </section>
 
     <section v-else>
-        <div v-if="cargando">
-            
-            <b-button variant="primary" disabled>
-                <b-spinner small type="grow"></b-spinner>
-                Cargando ...
-            </b-button>           
-            </div>
+        <div v-if="cargando" class="spinner">
+            Loading...
+        </div>
 
         <table class="table table-striped">
             <thead>
@@ -25,7 +21,10 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="todo in todos" :key="todo.id">
+                <tr 
+                    v-for="todo in todos"
+                    :key="todo.id"
+                >
                     <th scope="row">{{todo.id}}</th>
                     <td>{{todo.userId}}</td>
                     <td>{{todo.title}}</td>
@@ -70,6 +69,35 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.spinner {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    height:160px;
+    width:160px;
+    margin:0px auto;
+    -webkit-animation: rotation .6s infinite linear;
+    -moz-animation: rotation .6s infinite linear;
+    -o-animation: rotation .6s infinite linear;
+    animation: rotation .6s infinite linear;
+    border-left:6px solid rgba(0,174,239,.15);
+    border-right:6px solid rgba(0,174,239,.15);
+    border-bottom:6px solid rgba(0,174,239,.15);
+    border-top:6px solid rgba(0,174,239,.8);
+    border-radius:100%;
+}
 
+@-webkit-keyframes rotation {
+    from {-webkit-transform: rotate(0deg);}
+    to {-webkit-transform: rotate(359deg);}
+}
+@-moz-keyframes rotation {
+    from {-moz-transform: rotate(0deg);}
+    to {-moz-transform: rotate(359deg);}
+}
+@-o-keyframes rotation {
+    from {-o-transform: rotate(0deg);}
+    to {-o-transform: rotate(359deg);}
+}
 </style>
