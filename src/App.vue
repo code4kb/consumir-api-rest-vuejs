15c14c4b070f0c388cccd14ba46680382453d58f
Gabriel Rodriguez <code4kb@gmail.com>
2020-07-16 18:17:40 -0300

    Arreglo identación

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <div id="app">
     <div class="container">
-      <div class="row">      
+      <div class="row">
         <div class="col-xl-4">
           <div class="card bg-light">
             <h3 class="card-header font-weight-bold"><img alt="Vue logo" src="./assets/logo.png" class="imagen"> Vue.js + Axios.js</h3>
             <div class="card-body">
-              <h5 class="card-title text-left">Ejemplo sobre como consumir y mostrar datos desde un API REST a través de Vue.js, utilizando Axios.js.
-              </h5>
+              <h5 class="card-title text-left">Ejemplo sobre como consumir y mostrar datos desde un API REST a través de Vue.js, utilizando Axios.js.</h5>
             </div>
           </div>
         </div>
@@ -15,13 +14,11 @@
           <div class="card">
               <div class="card-header">
                 <h3 class="font-weight-bold"><img alt="Vue logo" src="./assets/logo.png" class="imagen"> Consumir servicio API REST - Vue.js</h3>
-
               </div>
               <div class="card-body">
                 <Listado />
-            </div> 
+              </div> 
           </div>
-          
         </div>
       </div>  
     </div>
