b3cb5aed1e77cd8fafffc05bd53e73754e269a31
Nelson Amador <rod6214@gmail.com>
2020-08-08 01:59:51 -0300

    Revert "Fixing lines"
    This reverts commit 0a4275c0704d87928cb7f0bf3f49a02a1f63a130.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,7 +13,6 @@
                     <li><a href="#" class="text-white">Follow on Twitter</a></li>
                     <li><a href="#" class="text-white">Like on Facebook</a></li>
                     <li><a href="#" class="text-white">Email me</a></li>
-                    
                 </ul>
                 </div>
             </div>
